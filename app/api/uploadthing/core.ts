b5ba17fd9f9be021ae45277a73fd5f8bec448aae
Vallabh Tiwari <tiwarivallabh27@gmail.com>
2025-03-15 01:34:50 +0530

    uploadthing.com added

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,28 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  pdfUploader: f({
+    pdf: {
+      maxFileSize: "16MB",
+    },
+  })
+    .middleware(async ({ req }) => {
+      const user = await currentUser();
+
+      if (!user) throw new UploadThingError("Unauthorized");
+
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("Upload complete for userId:", metadata.userId);
+      console.log("file url", file.ufsUrl);
+
+      return { uploadedBy: metadata.userId, fileUrl: file.ufsUrl };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
